1af5117ea2ac0eb356b6e20051f95c61e69577b4
AaronManuel15 <aaronmanuel15@gmail.com>
2023-01-07 17:10:33 -0600

    changed how I made this pycode compliant

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -8,7 +8,7 @@ def mat_mul(mat1, mat2):
     if len(mat1[0]) != len(mat2):
         return None
 
-    m = zip(*mat2)
-    matM = [[sum(a*b for a, b in zip(r, c)) for c in m] for r in mat1]
+    matM = [[sum(a*b for a, b in zip(row, col)) for col in zip(*mat2)]
+            for row in mat1]
 
     return matM
