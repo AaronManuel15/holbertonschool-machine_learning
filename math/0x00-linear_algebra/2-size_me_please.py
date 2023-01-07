59d1f1a7bfd3fda8a070724a211866503ea10793
AaronManuel15 <aaronmanuel15@gmail.com>
2023-01-06 18:40:41 -0600

    Task 2

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    """ Calculates the shape of a matrix"""
+    try:
+        return [len(matrix)] + matrix_shape(matrix[0])
+
+    except Exception:
+        return []
