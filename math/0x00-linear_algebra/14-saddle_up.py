a52efc1e9eed5df1e940c4232caf66f59168c0a5
AaronManuel15 <aaronmanuel15@gmail.com>
2023-01-07 18:06:28 -0600

    Task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Task 14"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """ Performs matrix multiplication"""
+
+    newMat = np.matmul(mat1, mat2)
+
+    return newMat
