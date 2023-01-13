b3e7a3727f71710c683209355d95d5acb97a3dc1
AaronManuel15 <aaronmanuel15@gmail.com>
2023-01-13 16:40:47 -0600

    Task 9 but no importing allowed

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ Task 9 for 0x02-Calculus"""
-import numpy as np
 
 
 def summation_i_squared(n):
@@ -9,12 +8,6 @@ def summation_i_squared(n):
     if type(n) is not int:
         return None
 
-    array = np.arange(1, n + 1, 1, dtype=int)
-    array = square(array)
-
-    return array.sum()
-
-
-def square(val):
-    """Had to do this for pycode"""
-    return val ** 2
+    if n > 0:
+        return ((n**2) + summation_i_squared(n - 1))
+    return 0
