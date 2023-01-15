a22ab57d4943b8ba7e72f53ffa19714f34eeba8b
AaronManuel15 <aaronmanuel15@gmail.com>
2023-01-15 00:29:13 -0600

    typecasting for removing trailing 0

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,10 +6,15 @@ def poly_integral(poly, C=0):
     """ Calculates the integral of poly"""
 
     if type(poly) is not list or type(C) not in [int, float] or len(poly) <= 0:
-        return None 
+        return None
+    if poly == [0]:
+        return [C]
 
     for power in range(1, len(poly)):
         poly[power] = (poly[power] / (power + 1))
+        if int(poly[power]) == float(poly[power]):
+            poly[power] = int(poly[power])
+
     poly.insert(0, C)
 
     return poly
