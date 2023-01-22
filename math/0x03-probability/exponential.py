30008e99148c55523552163e296f653042b07d41
AaronManuel15 <aaronmanuel15@gmail.com>
2023-01-21 22:07:52 -0600

    Task 3

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Task 3 - 5 for 0x03 - Probability"""
+pi = 3.1415926536
+e = 2.7182818285
+
+
+class Exponential():
+    """Represents an exponential distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """initializes the distribution"""
+
+        self.lambtha = float(lambtha)
+        if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) <= 1:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(1/(sum(data)/len(data)))
+        else:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
