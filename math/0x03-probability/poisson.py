4c4a66a8e17b0470266e85c510337f3513a068f1
AaronManuel15 <aaronmanuel15@gmail.com>
2023-01-21 20:04:30 -0600

    Task 0

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Task 0 for 0x03 - Probability"""
+
+
+class Poisson():
+    """Represents a poisson distribution"""
+    
+    def __init__(self, data=None, lambtha=1.):
+        
+        self.lambtha = float(lambtha)
+        if data:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) <= 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data)/len(data))
+        else:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
