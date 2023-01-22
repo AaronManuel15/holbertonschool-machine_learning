ba19920945fa8683ce45ac0b0609ea20a398779e
AaronManuel15 <aaronmanuel15@gmail.com>
2023-01-21 21:48:29 -0600

    Task 3

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -20,14 +20,31 @@ class Poisson():
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
 
+    def factorial(self, number):
+        """Performs factorial on number"""
+
+        if number == 0:
+            return 1
+        for i in range(1, int(number)):
+            number = number * i
+        return number
+
     def pmf(self, k):
         """Calculates the value of the PMF for a given number of “successes”"""
 
-        def factorial(number):
-            for i in range(1, int(number)):
-                number = number * i
-            return number
         k = int(k)
         if k < 1:
             return 0
-        return ((e ** -self.lambtha) * (self.lambtha ** k))/factorial(k)
+        return ((e ** -self.lambtha) * (self.lambtha ** k))/self.factorial(k)
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes”"""
+
+        k = int(k)
+        if k < 1:
+            return 0
+        cdf = 0
+        lmd = self.lambtha
+        for i in range(k + 1):
+            cdf += ((e ** -lmd) * (lmd ** i))/self.factorial(i)
+        return cdf
