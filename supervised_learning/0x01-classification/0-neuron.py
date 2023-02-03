931f2efc31c9aa0a1b8c3e465afa62e6baf58d5b
AaronManuel15 <aaronmanuel15@gmail.com>
2023-02-03 11:22:34 -0600

    Task 0

---
+++ supervised_learning/0x01-classification/0-neuron.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Task 0"""
+import numpy as np
+
+
+class Neuron():
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """initializes neuron, nx is number of input features"""
+
+        self.A = 0
+        self.b = 0
+
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(size=self.nz)
