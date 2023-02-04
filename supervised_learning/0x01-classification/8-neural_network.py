c41c9aab469d860adf339d83944265390e91947a
AaronManuel15 <aaronmanuel15@gmail.com>
2023-02-03 22:03:28 -0600

    Task 8

---
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Task 8"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """Defines a neural network network with one hidden layer
+    performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        """initializes network,
+        nx is number of input features
+        nodes is the number of nodes in the hidden layer"""
+
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.W1 = np.random.normal(size=[nodes, nx])
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=[1, nodes])
+        self.b2 = 0
+        self.A2 = 0
