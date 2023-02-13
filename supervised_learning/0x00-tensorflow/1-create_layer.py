5f1f2c01a24a6aa891260369fd743abc23452f4c
AaronManuel15 <aaronmanuel15@gmail.com>
2023-02-13 09:34:58 -0600

    Moved weight calculation into the dense call

--- supervised_learning/0x00-tensorflow/1-create_layer.py
+++ supervised_learning/0x00-tensorflow/1-create_layer.py
@@ -2,13 +2,12 @@
 """Task 1: creates layers of the NN"""
 
 import tensorflow as tf
-tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
 
 
 def create_layer(prev, n, activation):
     """ prev: tensor output of the previous layer
         n: number of nodes in the layer to be created
         activation: the activation function of the layer"""
-
-    lay = tf.layers.dense(prev, n, activation)
-    return lay
+    weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    return tf.layers.dense(prev, n, activation, kernal_regularizer = weights)
