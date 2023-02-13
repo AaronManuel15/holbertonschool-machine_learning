9827372f9ba95747ede44aee06afa7139b80bc26
AaronManuel15 <aaronmanuel15@gmail.com>
2023-02-13 09:42:30 -0600

    made regularizer an initializer

--- supervised_learning/0x00-tensorflow/1-create_layer.py
+++ supervised_learning/0x00-tensorflow/1-create_layer.py
@@ -4,10 +4,9 @@
 import tensorflow as tf
 
 
-
 def create_layer(prev, n, activation):
     """ prev: tensor output of the previous layer
         n: number of nodes in the layer to be created
         activation: the activation function of the layer"""
     weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    return tf.layers.dense(prev, n, activation, kernal_regularizer = weights)
+    return tf.layers.dense(prev, n, activation, kernel_initializer=weights)
