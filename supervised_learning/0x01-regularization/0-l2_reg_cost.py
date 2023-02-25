3cd9ad2bf8db18f6b17025d27c32edaedd967d72
AaronManuel15 <aaronmanuel15@gmail.com>
2023-02-25 17:52:44 -0600

    Added key guard to not include biases in calculation of weights

--- supervised_learning/0x01-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x01-regularization/0-l2_reg_cost.py
@@ -16,7 +16,8 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Returns: the cost of the network accounting for L2 regularization"""
 
     w2squared = 0
-    for _, w in weights.items():
-        w2squared += np.sum((w**2))
+    for key, array in weights.items():
+        if 'W' in key:
+            w2squared += np.sum((array**2))
 
     return cost + ((lambtha/(2*m)) * w2squared)
