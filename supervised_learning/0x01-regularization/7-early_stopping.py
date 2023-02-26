31515735873012df4c9ee9aee3244429d23b3735
AaronManuel15 <aaronmanuel15@gmail.com>
2023-02-26 08:02:02 -0600

    Task 7

---
+++ supervised_learning/0x01-regularization/7-early_stopping.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Task 7"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """Determines if gradient descent should be stopped early:
+
+    cost: current validation cost of the neural network
+    opt_cost: lowest recorded validation cost of the neural network
+    threshold: threshold used for early stopping
+    patience: patience count used for early stopping
+    count: count of how long the threshold has not been met
+
+    Returns: a boolean of whether the network should be stopped early,
+        followed by the updated count"""
+
+    if count + 1 >= patience:
+        return (True, count + 1)
+    elif cost >= opt_cost - threshold:
+        return (False, count + 1)
+    else:
+        return (False, 0)
