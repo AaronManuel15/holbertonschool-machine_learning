707ceed27368cb82b5d7eab65097450488d8fd13
AaronManuel15 <aaronmanuel15@gmail.com>
2023-02-26 07:15:56 -0600

    changed when the previous layer is fed and how keep_prob is applied to Dropout

--- supervised_learning/0x01-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x01-regularization/6-dropout_create_layer.py
@@ -15,9 +15,9 @@ def dropout_create_layer(prev, n, activation, keep_prob):
     Returns: the output of the new layer"""
 
     K_init = tf.contrib.layers.VarianceScaling(mode='FAN_AVG')
-    K_reg = tf.layers.Dropout(rate=(1-keep_prob))
+    K_reg = tf.layers.Dropout(1-keep_prob)
     NewLayer = tf.layers.Dense(units=n,
                                activation=activation,
                                kernel_initializer=K_init,
-                               kernel_regularizer=K_reg)(prev)
-    return NewLayer
+                               kernel_regularizer=K_reg)
+    return NewLayer(prev)
