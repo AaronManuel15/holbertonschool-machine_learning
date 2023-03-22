db87c95a1fbc5af3464572fa5d7db52e044db059
AaronManuel15 <aaronmanuel15@gmail.com>
2023-03-22 01:12:46 -0500

    Set stride to 2 for first conv layer

--- supervised_learning/0x01-deep_cnns/7-densenet121.py
+++ supervised_learning/0x01-deep_cnns/7-densenet121.py
@@ -18,12 +18,13 @@ def densenet121(growth_rate=32, compression=1.0):
     inputs = K.Input(shape=(224, 224, 3))
     bn1 = K.layers.BatchNormalization(axis=3)(inputs)
     act1 = K.layers.Activation('relu')(bn1)
-    conv1 = K.layers.Conv2D(filters=2 * growth_rate, kernel_size=(7, 7),
-                            kernel_initializer=init, padding='same')(act1)
+    conv1 = K.layers.Conv2D(filters=(2 * growth_rate), kernel_size=(7, 7),
+                            kernel_initializer=init, padding='same',
+                            strides=(2, 2))(act1)
     pool1 = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
                                   padding='same')(conv1)
 
-    block1, nb_filters = dense_block(pool1, 2 * growth_rate, growth_rate, 6)
+    block1, nb_filters = dense_block(pool1, (2 * growth_rate), growth_rate, 6)
     t_layer1, nb_filters = transition_layer(block1, nb_filters, compression)
     block2, nb_filters = dense_block(t_layer1, nb_filters, growth_rate, 12)
     t_layer2, nb_filters = transition_layer(block2, nb_filters, compression)
