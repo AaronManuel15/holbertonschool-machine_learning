889318d304874d2bcd639632f9e28c92f2af21cb
AaronManuel15 <aaronmanuel15@gmail.com>
2023-05-08 16:01:28 -0500

    Changed error checking again

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -2,28 +2,27 @@
 """Task 0: Determinant"""
 
 
-def get_mat_minor(m, i, j):
-    """Gets the minor of a matrix"""
-    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
-
-
 def determinant(matrix):
     """Calculates the determinant of a matrix
     Args:
         matrix (list): matrix to calculate"""
-    if matrix == [[]]:
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 1 and len(matrix[0]) == 0:
         return 1
-    if (type(matrix) and matrix is not list
-        or all(type(row) is not list for row in matrix)):
+    for i in range(len(matrix)):
+        if type(matrix[i]) is not list:
             raise TypeError("matrix must be a list of lists")
-    if len(matrix) != len(matrix[0]):
-        raise ValueError("matrix must be a square matrix")
-    if len(matrix) == 1:
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
-    if len(matrix) == 2:
-        return matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0]
+    if len(matrix) == 2 and len(matrix[0]) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     det = 0
-    for c in range(len(matrix)):
-        det += ((-1)**c)*matrix[0][c]*determinant(get_mat_minor(matrix, 0, c))
-
+    for i, k in enumerate(matrix[0]):
+        rows = [row for row in matrix[1:]]
+        new_m = [[row[n] for n in range(len(matrix)) if n != i]
+                 for row in rows]
+        det += k * (-1) ** i * determinant(new_m)
     return det
