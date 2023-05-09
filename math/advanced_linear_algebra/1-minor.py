2ef8897277259de16387a1d57efabf884f8b7823
AaronManuel15 <aaronmanuel15@gmail.com>
2023-05-09 11:10:09 -0500

    changed it up

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -39,20 +39,13 @@ def minor(matrix):
         matrix (list): matrix to calculate"""
     if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a list of lists")
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError("matrix must be a list of lists")
-    if len(matrix) == 1:
-        if len(matrix[0]) == 0:
-            raise ValueError("matrix must be a non-empty square matrix")
-        return [[1]]
-    if len(matrix) != len(matrix[0]):
-        raise ValueError("matrix must be a non-empty square matrix")
-    if len(matrix) == 2:
-        return [[matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]]
-    minor = []
     for i in range(len(matrix)):
-        minor.append([])
-        for j in range(len(matrix)):
-            minor[i].append(determinant(get_matrix_minor(matrix, i, j)))
-    return minor
+        if type(matrix[i]) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a non-empty square matrix")
+    if len(matrix) == 1:
+        return [[1]]
+    return [[determinant(get_matrix_minor(matrix, i, j))
+             for j in range(len(matrix[0]))]
+            for i in range(len(matrix))]
