66b720bf959e10c646a538ca4461828e16f0fb3d
AaronManuel15 <aaronmanuel15@gmail.com>
2023-05-12 13:01:53 -0500

    Task 1: Correlation

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Task 1: Correlation"""
+import numpy as np
+
+
+def correlation(C):
+    """Calculates a correlation matrix:
+    Args:
+        C (np.ndarray): covariance matrix with shape of (d, d) where
+        d is the number of dimensions"""
+    if type(C) is not np.ndarray:
+        raise TypeError("C must be a numpy.ndarray")
+    if C.shape[0] != C.shape[1] or len(C.shape) != 2:
+        raise ValueError("C must be a 2D square matrix")
+    var = np.sqrt(np.diag(C))
+    return np.divide(C, np.outer(var, var))
