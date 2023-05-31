d0603c1092657ebf2f24ce4b8cf33ac00533d89c
AaronManuel15 <aaronmanuel15@gmail.com>
2023-05-31 15:43:47 -0500

    Task 12

---
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -0,0 +1,19 @@
+"""Task 12: Agglomerative"""
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """Performs agglomerative clustering on a dataset with Ward linkage and
+        displays the dendrogram
+    Args:
+        X: np.ndarray of shape (n, d) containing the dataset
+        dist: maximum cophenetic distance for all clusters
+    Returns: clss"""
+
+    Z = scipy.cluster.hierarchy.linkage(X, method='ward')
+    clss = scipy.cluster.hierarchy.fcluster(Z, dist, criterion='distance')
+    scipy.cluster.hierarchy.dendrogram(Z, color_threshold=dist,
+                                       above_threshold_color='b')
+    plt.show()
+    return clss
