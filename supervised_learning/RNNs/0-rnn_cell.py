9969c09540dec29a0a85f7151c8b2e0822fe67d1
AaronManuel15 <aaronmanuel15@gmail.com>
2023-07-10 21:06:07 -0500

    pycodestyle

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -17,8 +17,10 @@ class RNNCell:
 
     def forward(self, h_prev, x_t):
         """Performs forward propagation for one time step
-            h_prev: numpy.ndarray of shape (m, h) containing the previous hidden state
-            x_t: numpy.ndarray of shape (m, i) containing the data input for the cell
+            h_prev: numpy.ndarray of shape (m, h) containing the previous
+                hidden state
+            x_t: numpy.ndarray of shape (m, i) containing the data input for
+                the cell
                 m: batch size for the data
             Returns: h_next, y
                 h_next: next hidden state
