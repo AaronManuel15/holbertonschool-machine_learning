1f550a07b557e76314c28ed4092c1f685928ed59
AaronManuel15 <aaronmanuel15@gmail.com>
2023-07-24 14:26:51 -0500

    Adding blank files for Task 0 so I can test this checker format

---
+++ supervised_learning/time_series/forecast_btc.py
@@ -0,0 +1,4 @@
+"""Task 0 - Preprocess data
+Takes processed data from supervised_learning/time_series/preprocess_data.py
+and performs time series forecasting of Bitcoin prices.
+Creates, trains, and validates a keras model."""
---
+++ supervised_learning/time_series/preprocess_data.py
@@ -0,0 +1,3 @@
+"""Task 0 - Preprocess data
+Script to preprocess raw data for time series forecasting of
+Bitcoin prices using coinbase and bitstamp datasets."""
