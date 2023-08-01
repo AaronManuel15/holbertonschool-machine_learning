fd2785b35bafbb6928ac6bab33a2e90b9663dccc
AaronManuel15 <aaronmanuel15@gmail.com>
2023-08-01 05:25:24 -0500

    Task 0: Bag of Words

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Task 0:  Bag of Words"""
+import string
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def bag_of_words(sentences, vocab=None):
+    """creates a bag of words embedding matrix
+    Args:
+        sentences: list of sentences to analyze
+        vocab: list of the vocabulary words to use for the analysis
+    Returns:
+        embeddings, features
+        embeddings: the embeddings matrix
+        features: the feature names
+    """
+
+    translator = str.maketrans('', '', string.punctuation)
+    features = []
+
+    # lowercase it all
+    sentences = [i.lower() for i in sentences]
+    sentences = [i.replace("'s", '') for i in sentences]
+
+    # remove punctuation
+    for i in range(len(sentences)):
+        sentences[i] = sentences[i].translate(translator)
+
+    corpus = sentences.copy()
+
+    # split words up
+    for elem in sentences:
+        append = elem.split()
+        features.extend(append)
+
+    features = sorted(list(set(features)))
+
+    if vocab is not None:
+        features = [x for x in features if x in vocab]
+
+    cv = CountVectorizer(vocabulary=features)
+    embedding = cv.fit_transform(corpus).toarray()
+
+    return embedding, features
