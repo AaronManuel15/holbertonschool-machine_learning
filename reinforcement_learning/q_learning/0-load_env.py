6e608151c2e6aa1cb5bdc36b13a353ac9a755076
AaronManuel15 <aaronmanuel15@gmail.com>
2023-09-10 23:25:59 -0500

    Changing the v0 back to v1 as this is manually reviewed

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -5,7 +5,7 @@ import gym
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """Load the FrozenLakeEnv environment from OpenAI Gym"""
-    env = gym.make("FrozenLake-v0",
+    env = gym.make("FrozenLake-v1",
                    desc=desc,
                    map_name=map_name,
                    is_slippery=is_slippery)
