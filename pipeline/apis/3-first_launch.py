1d1683a30fb57ca2db49f602adcd56b6ebbb9efa
AaronManuel15 <aaronmanuel15@gmail.com>
2023-10-17 13:54:44 -0500

    Pushing to test checker on what file is needed

---
+++ pipeline/apis/3-first_launch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Task 3. First launch"""
+
+if __name__ == '__main__':
+    """Initializing script on use"""
+    import requests
+    import sys
+    import time
+
+    user = requests.get()
