e57eef75aad0faae46f4b81bb3cdb15886dfe7c6
AaronManuel15 <aaronmanuel15@gmail.com>
2023-10-17 13:34:44 -0500

    changed 404 error handling and fixed Rate Limit value issues

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """Task 2. Rate me is you can!"""
-import requests
-import sys
 
 if __name__ == '__main__':
     """Initializing script on use"""
+    import requests
+    import sys
+    import time
 
     user = requests.get(sys.argv[1])
 
     if user.status_code == 200:
         print(user.json()['location'])
     elif user.status_code == 403:
-        print('Reset in {} min'.format(user.headers['X-RateLimit-Reset'] / 60))
+        time = (int(user.headers['X-RateLimit-Reset']) - int(time.time())) / 60
+        print('Reset in {} min'.format(time))
     if user.status_code == 404:
-        if user.json()['message'] == 'Not Found':
-            print('Not found')
+        print('Not found')
